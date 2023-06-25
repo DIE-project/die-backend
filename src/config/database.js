1111bc0b07c6d5175399c14c626c6c496f6770ea
jdelaguila-dev <jdelaguila@devcloud.pe>
2023-06-25 15:21:22 -0500

    v3

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,9 @@
 import { Sequelize } from "sequelize";
-import sqlite3 from "sqlite3";
 import { DB_NAME, DB_PASSWORD, DB_USER } from "../config.js";
 
 const db = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: "localhost",
-  dialect: "sqlite",
+  dialect: "postgres",
 });
 
 export default db;
